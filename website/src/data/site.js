8a864f929d76881ca31551c1a8c0808245b0e6a5
Aman Mishra <amanrox97@gmail.com>
2026-08-01 17:17:10 +0400

    Added Onshape CAD document and Overleaf report links to the site data file.

--- website/src/data/site.js
+++ website/src/data/site.js
@@ -52,14 +52,14 @@ export const files = [
     key: "onshape",
     title: "Onshape CAD Document",
     desc: "The live, parametric CAD assembly.",
-    url: "#",
+    url: "https://cad.onshape.com/documents/26ae62bdc068d3a1a757a32c/w/bd8636f8197cb10e6369b7c2/e/67a251683f5a185aeeb3ceee?renderMode=0&uiState=6a6dece1a4b76ae5157c6445",
     accent: "blue",
   },
   {
     key: "report",
     title: "Dissertation Report",
     desc: "Written report repository (LaTeX).",
-    url: "#",
+    url: "https://www.overleaf.com/read/fqxmxhshbhdr#3005a3",
     accent: "yellow",
   },
   {
